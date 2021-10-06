e5a0ebcd51295afd2266d05affb434f94661c359
bechir000 <75615912+bechir000@users.noreply.github.com>
2021-10-06 23:22:33 +0200

    Add files via upload

---
+++ python arrays.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+student1 = "john"
+student2 = "jimmy"
+student3 = "chris"
+
+
+
+Get the value of the first array item:
+x = student[5]
+
+
+Return the number of elements in the students array:
+x = len(students)
+
+
+Print each item in the cars array:
+    for x in students:
+     print(x)
+    
+
+Add one more element to the cars array:
+students.append("Bechir")
+
+
+Delete the second element of the students array:
+students.pop(1)
+
+
+# In[ ]:
+
+
+
+
